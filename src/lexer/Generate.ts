930359d4e96452b7fac6c36c2964d0e3d36b2f86
z0gSh1u <zx.cs@qq.com>
2020-11-09 22:35:01 +0800

    修改Generate相关代码

--- src/lexer/Generate.ts
+++ src/lexer/Generate.ts
@@ -1,14 +1,23 @@
+/**
+ * 从.l文件生成序列化的DFA
+ */
+
 import { DFA } from '../seu-lex-yacc/seulex/DFA'
 import { LexParser } from '../seu-lex-yacc/seulex/LexParser'
 import { NFA } from '../seu-lex-yacc/seulex/NFA'
 import * as path from 'path'
+import { assert } from '../seu-lex-yacc/utils'
 
-const dfa = DFA.fromNFA(NFA.fromLexParser(new LexParser(path.join(__dirname, '../../src/lexer/MiniC.l'))))
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const args = require('minimist')(process.argv.slice(2))
+// args looks like { _: [ 'example/md.l' ], v: true }
+assert(args._.length == 2, '[usage]: node Generate.js <path_to_.l> <path_output>')
+const dotLPath = args._[0]
+const dotLName = path.basename(dotLPath)
+const outJSONPath = args._[1]
+
+const dfa = DFA.fromNFA(NFA.fromLexParser(new LexParser(dotLPath)))
 dfa.dump(
-  `Generated from MiniC.l @ ${new Date().toLocaleDateString()}`,
-  path.join(__dirname, '../../src/lexer/MiniC-Lex.json')
-)
-dfa.dump(
-  `Generated from MiniC.l @ ${new Date().toLocaleDateString()}`,
-  path.join(__dirname, '../../dist/lexer/MiniC-Lex.json')
+  `Generated from ${dotLName} @ ${new Date().toLocaleDateString()}`,
+  path.join(outJSONPath, dotLName + '-Lex.json')
 )
---
+++ src/parser/GenerateLR1.ts
@@ -0,0 +1,22 @@
+/**
+ * 从.y文件生成序列化的LR1Analyzer
+ */
+
+import { LR1Analyzer } from '../seu-lex-yacc/seuyacc/LR1'
+import { YaccParser } from '../seu-lex-yacc/seuyacc/YaccParser'
+import * as path from 'path'
+import { assert } from '../seu-lex-yacc/utils'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const args = require('minimist')(process.argv.slice(2))
+// args looks like { _: [ 'example/md.l' ], v: true }
+assert(args._.length == 2, '[usage]: node GenerateLR1.js <path_to_.y> <path_output>')
+const dotYPath = args._[0]
+const dotYName = path.basename(dotYPath)
+const outJSONPath = args._[1]
+
+const lr1 = new LR1Analyzer(new YaccParser(dotYPath))
+lr1.dump(
+  `Generated from ${dotYName} @ ${new Date().toLocaleDateString()}`,
+  path.join(outJSONPath, dotYName + '-Yacc.json')
+)
