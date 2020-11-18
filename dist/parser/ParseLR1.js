9202870d51eab3b91de74725eccc577efc95ac05
Withod <withod@foxmail.com>
2020-11-18 17:10:56 +0800

    feat: 完善了语法分析中对属性值的存取

--- dist/parser/ParseLR1.js
+++ dist/parser/ParseLR1.js
@@ -62,6 +62,26 @@ function parseTokensLR1(tokens, analyzer) {
         }
         return table;
     })();
+    // 属性值栈
+    const literalStack = [];
+    let curRhsLen = 0;
+    let curLiteral = { name: '', literal: '' };
+    /**
+     * 以Token的形式获取当前归约产生式右侧符号的属性值
+     * @param num 符号在产生式右侧的序号，例如取$2则num传2
+     */
+    function getLiteral(num) {
+        utils_1.assert(num > 0 && num <= curRhsLen, `动作代码中存在错误的属性值引用：$${num}`);
+        return literalStack.slice(num - curRhsLen - 1)[0];
+    }
+    /**
+     * 暂存当前产生式左侧符号的属性值（即$$）
+     * 调用该函数后不会立刻改变属性值栈，而是在完成此次归约后真正存储
+     * @param literal 所要存储的$$的值
+     */
+    function setLiteral(literal) {
+        curLiteral = { name: 'nonterminal', literal: literal };
+    }
     // 状态栈
     const stateStack = [analyzer.dfa.startStateId];
     // 处理当前情况遇到symbol
@@ -69,12 +89,15 @@ function parseTokensLR1(tokens, analyzer) {
         if (symbol === exports.WHITESPACE_SYMBOL_ID)
             return symbol;
         switch (table[stateStack.slice(-1)[0]][symbol].action) {
-            case 'nonterminal':
             case 'shift':
+                literalStack.push(tokens[symbol]);
+            case 'nonterminal':
                 stateStack.push(table[stateStack.slice(-1)[0]][symbol].target);
                 return symbol;
             case 'reduce':
                 let producer = analyzer.producers[table[stateStack.slice(-1)[0]][symbol].target];
+                curRhsLen = producer.rhs.length;
+                curLiteral = literalStack.slice(-curRhsLen)[0];
                 const execAction = () => {
                     // TODO: 在此添加动作代码的执行逻辑
                     const actionCode = producer.action; // 动作代码
@@ -82,9 +105,9 @@ function parseTokensLR1(tokens, analyzer) {
                     console.log(analyzer.formatPrintProducer(producer));
                 };
                 execAction();
-                let i = producer.rhs.length;
-                while (i--)
-                    stateStack.pop();
+                while (curRhsLen--)
+                    stateStack.pop(), literalStack.pop();
+                literalStack.push(curLiteral);
                 return producer.lhs;
             case 'acc':
                 return -1;
