60f3d32bb44c130440b0398eb0e5ec5f0e6a154c
z0gSh1u <zx.cs@qq.com>
2021-01-13 15:54:36 +0800

    改进报错

--- dist/cli.js
+++ dist/cli.js
@@ -44,65 +44,75 @@ const print = (message, style = '') => {
         return;
     eval(`console.log(chalk${style.trim() ? '.' : ''}${style.split(' ').join('.')}('${message}'))`);
 };
-const tik = new Date().getTime();
-print(`                                    `, 'bgWhite bold black');
-print(`  ===== [minisys-minicc-ts] ======  `, 'bgWhite bold black');
-print(`                                    `, 'bgWhite bold black');
-print(`*** Basic Information ***`, 'bgBlue bold white');
-print(`  Source: ${codePath}`);
-print(`  Output Path: ${outputPath}`);
-print(`  With IR: ${String(withIR)}`);
-print('*** Start frontend part... ***', 'bgBlue bold white');
-// 读入C源码
-print('  Reading source file...', 'yellow');
-const rawCCode = fs_1.default.readFileSync(codePath).toString('utf-8');
-assert(rawCCode.trim(), 'Source code is empty!');
-print('  Source file loaded.', 'green');
-// 预编译
-print('  Start precompiling...', 'yellow');
-const CCode = PreCompile_1.preCompile(rawCCode, path_1.default.dirname(codePath));
-print('  Precompilation done.', 'green');
-// 词法分析
-print('  Loading DFA for lexing...', 'yellow');
-const lexDFADumpPath = path_1.default.join(__dirname, '../syntax/MiniC/MiniC-Lex.json');
-const lexDFA = DFA_1.DFA.fromFile(lexDFADumpPath);
-print('  Lexing DFA loaded from ' + lexDFADumpPath.replace(/\\/g, '/'), 'green');
-print('  Start tokenization...', 'yellow');
-const tokens = Lex_1.lexSourceCode(CCode, lexDFA);
-print('  Tokenization done. Received ' + tokens.length + ' tokens.', 'green');
-// 语法分析
-print('  Loading parsing table for parsing...', 'yellow');
-const lalrDumpPath = path_1.default.join(__dirname, '../syntax/MiniC/MiniC-LALRParse.json');
-const lalr = LALR_1.LALRAnalyzer.load(lalrDumpPath);
-print('  Parsing table loaded from ' + lalrDumpPath.replace(/\\/g, '/'), 'green');
-print('  Start parsing...', 'yellow');
-const astRoot = ParseLALR_1.parseTokensLALR(tokens, lalr);
-assert(astRoot, '  AST root is null.');
-print('  Parsing done.', 'green');
-print('*** Start backend part... ***', 'bgBlue bold white');
-// 中间代码生成
-print('  Generating Intermediate Representation...', 'yellow');
-const ir = new IRGenerator_1.IRGenerator(astRoot);
-print('  Intermediate Representation generation done.', 'green');
-// 中间代码优化
-print('  Optimizing Intermediate Representation...', 'yellow');
-const opt = new IROptimizer_1.IROptimizer(ir);
-print(`  IR optimization done. Made ${opt.printLogs().split('\n').length} optimizations.`, 'green');
-// 目标代码生成与优化
-print('  Generating object code (assembly code)...', 'yellow');
-const asm = new ASMGenerator_1.ASMGenerator(ir);
-print('  Object code generation done.', 'green');
-// 输出
-print('  Start output works...', 'yellow');
-const asmCode = asm.toAssembly();
-fs_1.default.writeFileSync(path_1.default.join(outputPath, outputName + '.asm'), asmCode);
-print('  Object code output successfully.', 'green');
-if (withIR) {
-    const irCode = ir.toIRString();
-    fs_1.default.writeFileSync(path_1.default.join(outputPath, outputName + '.ir'), irCode);
-    print('  Intermediate Representation output successfully.', 'green');
+try {
+    const tik = new Date().getTime();
+    print(`                                    `, 'bgWhite bold black');
+    print(`  ===== [minisys-minicc-ts] ======  `, 'bgWhite bold black');
+    print(`                                    `, 'bgWhite bold black');
+    print(`*** Basic Information ***`, 'bgBlue bold white');
+    print(`  Source: ${codePath}`);
+    print(`  Output Path: ${outputPath}`);
+    print(`  With IR: ${String(withIR)}`);
+    print('*** Start frontend part... ***', 'bgBlue bold white');
+    // 读入C源码
+    print('  Reading source file...', 'yellow');
+    const rawCCode = fs_1.default.readFileSync(codePath).toString('utf-8');
+    assert(rawCCode.trim(), 'Source code is empty!');
+    print('  Source file loaded.', 'green');
+    // 预编译
+    print('  Start precompiling...', 'yellow');
+    const CCode = PreCompile_1.preCompile(rawCCode, path_1.default.dirname(codePath));
+    print('  Precompilation done.', 'green');
+    // 词法分析
+    print('  Loading DFA for lexing...', 'yellow');
+    const lexDFADumpPath = path_1.default.join(__dirname, '../syntax/MiniC/MiniC-Lex.json');
+    const lexDFA = DFA_1.DFA.fromFile(lexDFADumpPath);
+    print('  Lexing DFA loaded from ' + lexDFADumpPath.replace(/\\/g, '/'), 'green');
+    print('  Start tokenization...', 'yellow');
+    const tokens = Lex_1.lexSourceCode(CCode, lexDFA);
+    print('  Tokenization done. Received ' + tokens.length + ' tokens.', 'green');
+    // 语法分析
+    print('  Loading parsing table for parsing...', 'yellow');
+    const lalrDumpPath = path_1.default.join(__dirname, '../syntax/MiniC/MiniC-LALRParse.json');
+    const lalr = LALR_1.LALRAnalyzer.load(lalrDumpPath);
+    print('  Parsing table loaded from ' + lalrDumpPath.replace(/\\/g, '/'), 'green');
+    print('  Start parsing...', 'yellow');
+    const astRoot = ParseLALR_1.parseTokensLALR(tokens, lalr);
+    assert(astRoot, '  AST root is null.');
+    print('  Parsing done.', 'green');
+    print('*** Start backend part... ***', 'bgBlue bold white');
+    // 中间代码生成
+    print('  Generating Intermediate Representation...', 'yellow');
+    const ir = new IRGenerator_1.IRGenerator(astRoot);
+    print('  Intermediate Representation generation done.', 'green');
+    // 中间代码优化
+    print('  Optimizing Intermediate Representation...', 'yellow');
+    const opt = new IROptimizer_1.IROptimizer(ir);
+    print(`  IR optimization done. Made ${opt.printLogs().split('\n').length} optimizations.`, 'green');
+    // 目标代码生成与优化
+    print('  Generating object code (assembly code)...', 'yellow');
+    const asm = new ASMGenerator_1.ASMGenerator(ir);
+    print('  Object code generation done.', 'green');
+    // 输出
+    print('  Start output works...', 'yellow');
+    const asmCode = asm.toAssembly();
+    fs_1.default.writeFileSync(path_1.default.join(outputPath, outputName + '.asm'), asmCode);
+    print('  Object code output successfully.', 'green');
+    if (withIR) {
+        const irCode = ir.toIRString();
+        fs_1.default.writeFileSync(path_1.default.join(outputPath, outputName + '.ir'), irCode);
+        print('  Intermediate Representation output successfully.', 'green');
+    }
+    print('  Output works done.', 'green');
+    const tok = new Date().getTime();
+    print('*** Summary ***', 'bgBlue bold white');
+    print('  Compilation ended successfully with in ' + String((tok - tik) / 1000) + ' secs.');
 }
-print('  Output works done.', 'green');
-const tok = new Date().getTime();
-print('*** Summary ***', 'bgBlue bold white');
-print('  Compilation ended successfully with in ' + String((tok - tik) / 1000) + ' secs.');
+catch (ex) {
+    if (ex instanceof utils_1.SeuError) {
+        print('[SeuError]' + ex.message, 'red bold');
+    }
+    else {
+        throw ex;
+    }
+}
--- src/cli.ts
+++ src/cli.ts
@@ -10,7 +10,7 @@
 
 import path from 'path'
 import fs from 'fs'
-import { assert as assert_ } from './seu-lex-yacc/utils'
+import { assert as assert_, SeuError } from './seu-lex-yacc/utils'
 import { lexSourceCode } from './lexer/Lex'
 import { DFA } from './seu-lex-yacc/seulex/DFA'
 import { LALRAnalyzer } from './seu-lex-yacc/seuyacc/LALR'
@@ -43,79 +43,87 @@ const print = (message: string, style: string = '') => {
   eval(`console.log(chalk${style.trim() ? '.' : ''}${style.split(' ').join('.')}('${message}'))`)
 }
 
-const tik = new Date().getTime()
+try {
+  const tik = new Date().getTime()
 
-print(`                                    `, 'bgWhite bold black')
-print(`  ===== [minisys-minicc-ts] ======  `, 'bgWhite bold black')
-print(`                                    `, 'bgWhite bold black')
+  print(`                                    `, 'bgWhite bold black')
+  print(`  ===== [minisys-minicc-ts] ======  `, 'bgWhite bold black')
+  print(`                                    `, 'bgWhite bold black')
 
-print(`*** Basic Information ***`, 'bgBlue bold white')
-print(`  Source: ${codePath}`)
-print(`  Output Path: ${outputPath}`)
-print(`  With IR: ${String(withIR)}`)
+  print(`*** Basic Information ***`, 'bgBlue bold white')
+  print(`  Source: ${codePath}`)
+  print(`  Output Path: ${outputPath}`)
+  print(`  With IR: ${String(withIR)}`)
 
-print('*** Start frontend part... ***', 'bgBlue bold white')
+  print('*** Start frontend part... ***', 'bgBlue bold white')
 
-// 读入C源码
-print('  Reading source file...', 'yellow')
-const rawCCode = fs.readFileSync(codePath).toString('utf-8')
-assert(rawCCode.trim(), 'Source code is empty!')
-print('  Source file loaded.', 'green')
+  // 读入C源码
+  print('  Reading source file...', 'yellow')
+  const rawCCode = fs.readFileSync(codePath).toString('utf-8')
+  assert(rawCCode.trim(), 'Source code is empty!')
+  print('  Source file loaded.', 'green')
 
-// 预编译
-print('  Start precompiling...', 'yellow')
-const CCode = preCompile(rawCCode, path.dirname(codePath))
-print('  Precompilation done.', 'green')
+  // 预编译
+  print('  Start precompiling...', 'yellow')
+  const CCode = preCompile(rawCCode, path.dirname(codePath))
+  print('  Precompilation done.', 'green')
 
-// 词法分析
-print('  Loading DFA for lexing...', 'yellow')
-const lexDFADumpPath = path.join(__dirname, '../syntax/MiniC/MiniC-Lex.json')
-const lexDFA = DFA.fromFile(lexDFADumpPath)
-print('  Lexing DFA loaded from ' + lexDFADumpPath.replace(/\\/g, '/'), 'green')
-print('  Start tokenization...', 'yellow')
-const tokens = lexSourceCode(CCode, lexDFA)
-print('  Tokenization done. Received ' + tokens.length + ' tokens.', 'green')
+  // 词法分析
+  print('  Loading DFA for lexing...', 'yellow')
+  const lexDFADumpPath = path.join(__dirname, '../syntax/MiniC/MiniC-Lex.json')
+  const lexDFA = DFA.fromFile(lexDFADumpPath)
+  print('  Lexing DFA loaded from ' + lexDFADumpPath.replace(/\\/g, '/'), 'green')
+  print('  Start tokenization...', 'yellow')
+  const tokens = lexSourceCode(CCode, lexDFA)
+  print('  Tokenization done. Received ' + tokens.length + ' tokens.', 'green')
 
-// 语法分析
-print('  Loading parsing table for parsing...', 'yellow')
-const lalrDumpPath = path.join(__dirname, '../syntax/MiniC/MiniC-LALRParse.json')
-const lalr = LALRAnalyzer.load(lalrDumpPath)
-print('  Parsing table loaded from ' + lalrDumpPath.replace(/\\/g, '/'), 'green')
-print('  Start parsing...', 'yellow')
-const astRoot = parseTokensLALR(tokens, lalr)
-assert(astRoot, '  AST root is null.')
-print('  Parsing done.', 'green')
+  // 语法分析
+  print('  Loading parsing table for parsing...', 'yellow')
+  const lalrDumpPath = path.join(__dirname, '../syntax/MiniC/MiniC-LALRParse.json')
+  const lalr = LALRAnalyzer.load(lalrDumpPath)
+  print('  Parsing table loaded from ' + lalrDumpPath.replace(/\\/g, '/'), 'green')
+  print('  Start parsing...', 'yellow')
+  const astRoot = parseTokensLALR(tokens, lalr)
+  assert(astRoot, '  AST root is null.')
+  print('  Parsing done.', 'green')
 
-print('*** Start backend part... ***', 'bgBlue bold white')
+  print('*** Start backend part... ***', 'bgBlue bold white')
 
-// 中间代码生成
-print('  Generating Intermediate Representation...', 'yellow')
-const ir = new IRGenerator(astRoot!)
-print('  Intermediate Representation generation done.', 'green')
+  // 中间代码生成
+  print('  Generating Intermediate Representation...', 'yellow')
+  const ir = new IRGenerator(astRoot!)
+  print('  Intermediate Representation generation done.', 'green')
 
-// 中间代码优化
-print('  Optimizing Intermediate Representation...', 'yellow')
-const opt = new IROptimizer(ir)
-print(`  IR optimization done. Made ${opt.printLogs().split('\n').length} optimizations.`, 'green')
+  // 中间代码优化
+  print('  Optimizing Intermediate Representation...', 'yellow')
+  const opt = new IROptimizer(ir)
+  print(`  IR optimization done. Made ${opt.printLogs().split('\n').length} optimizations.`, 'green')
 
-// 目标代码生成与优化
-print('  Generating object code (assembly code)...', 'yellow')
-const asm = new ASMGenerator(ir)
-print('  Object code generation done.', 'green')
+  // 目标代码生成与优化
+  print('  Generating object code (assembly code)...', 'yellow')
+  const asm = new ASMGenerator(ir)
+  print('  Object code generation done.', 'green')
 
-// 输出
-print('  Start output works...', 'yellow')
-const asmCode = asm.toAssembly()
-fs.writeFileSync(path.join(outputPath, outputName + '.asm'), asmCode)
-print('  Object code output successfully.', 'green')
-if (withIR) {
-  const irCode = ir.toIRString()
-  fs.writeFileSync(path.join(outputPath, outputName + '.ir'), irCode)
-  print('  Intermediate Representation output successfully.', 'green')
+  // 输出
+  print('  Start output works...', 'yellow')
+  const asmCode = asm.toAssembly()
+  fs.writeFileSync(path.join(outputPath, outputName + '.asm'), asmCode)
+  print('  Object code output successfully.', 'green')
+  if (withIR) {
+    const irCode = ir.toIRString()
+    fs.writeFileSync(path.join(outputPath, outputName + '.ir'), irCode)
+    print('  Intermediate Representation output successfully.', 'green')
+  }
+  print('  Output works done.', 'green')
+
+  const tok = new Date().getTime()
+
+  print('*** Summary ***', 'bgBlue bold white')
+  print('  Compilation ended successfully with in ' + String((tok - tik) / 1000) + ' secs.')
+} catch (ex) {
+  if (ex instanceof SeuError) {
+    print('[SeuError]' + ex.message, 'red bold')
+  } else {
+    throw ex
+  }
 }
-print('  Output works done.', 'green')
-
-const tok = new Date().getTime()
-
-print('*** Summary ***', 'bgBlue bold white')
-print('  Compilation ended successfully with in ' + String((tok - tik) / 1000) + ' secs.')
